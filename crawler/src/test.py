d489adff11ab58a7626750c274cad8533c926df2
r_r <ruiiwa30@gmail.com>
2022-02-04 22:42:09 +0900

    test

--- crawler/src/test.py
+++ crawler/src/test.py
@@ -2,9 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 # Webページを取得して解析する
 
-load_url = "https://www.ymori.com/books/python2nen/test1.html"
-html = requests.get(load_url)
+load_url = "https://www.fujiya-avic.co.jp/shop/goods/search.aspx?sort=5&genre=A-HDP&category=40&search=x&stock=1"
+dummy_user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
+html = requests.get(load_url, headers={"User-Agent": dummy_user_agent})
+
 soup = BeautifulSoup(html.content, "html.parser")
 
-# HTML全体を表示する
-print(soup)
+lists = []
+itemNames = soup.find_all("div", {"class": "block-thumbnail-t--goods-name"})
+for itemName in itemNames:
+    lists.append(itemName.get_text().strip())
+
+for name in lists:
+    print(name)
